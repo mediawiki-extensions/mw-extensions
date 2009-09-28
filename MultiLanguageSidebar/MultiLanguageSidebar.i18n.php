e495113819ebb4a0a6bd86e27fae9b1d889cde0b
sorin.sbarnea <sorin.sbarnea@8dc03fec-ac5d-11de-a87c-5360b77f26dc>
2009-09-28 19:07:52 +0000

    Added MultiLanguageSidebar (to be used with MultiLanguageManager)

---
+++ MultiLanguageSidebar/MultiLanguageSidebar.i18n.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Created on 29 janv. 2008
+ *
+ * http://www.art122-5.net
+ * Marc Despland 
+ * 
+ * Copyright (C) 2007  Marc Despland
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+ $allMessages = array(
+	'en' => array( 
+		'presentation-url' => 'General Presentation',
+		'presentation' => 'Presentation',
+ 		'teaching-url' => 'Teaching',
+		'teaching' => 'Teaching',
+		'team-url' => 'Team',
+		'team' => 'Team',
+ 		'research-url' => 'Research and projects',
+		'research' => 'Research and projects',
+		'publication-url' => 'Publications (En)',
+		'publication' => 'Publications',
+ 		'memories-url' => 'Memories',
+		'memories' => 'Memories',
+		'contact-url' => 'Contact',
+		'contact' => 'Contact'
+	),
+	'fr' => array( 
+		'presentation-url' => 'Présentation générale',
+		'presentation' => 'Présentation',
+ 		'teaching-url' => 'Activités d\'enseignement',
+		'teaching' => 'Enseignement',
+		'team-url' => 'Membres de l\'unité',
+		'team' => 'Membres',
+ 		'research-url' => 'Recherches et projets',
+		'research' => 'Recherches et projets',
+		'publication-url' => 'Publications',
+		'publication' => 'Publications',
+ 		'memories-url' => 'MFE & DEA',
+		'memories' => 'MFE & DEA',
+		'contact-url' => 'Adresse et localisation',
+		'contact' => 'Adresse et localisation'
+	)
+);
+ 
+?>
